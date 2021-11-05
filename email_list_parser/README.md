9ca455cbb862ffe517de725416d3e1a13dd0ac7b
Jan Matuszewski <matuszewski.jan@hotmail.com>
2021-11-05 15:32:20 +0100

    Update email_list_parser task with more detailed description and specs

--- email_list_parser/README.md
+++ email_list_parser/README.md
@@ -1,4 +1,6 @@
-Hey, here is list of emails of our employees `[support/email-list.txt]`.
+Hey, here is list of emails of our employees `[support/email-list-new.txt]`.
 We always used this simple script to create a CSV with first/last names and emails.
 No idea why but the output for current list seems to be completely wrong.
-Could you take a look to fix it? Don't hesistate to edit the list if there is something wrong with it's format.
+Could you take a look to fix it?
+
+I am also sending old list which were working great(`[support/email-list-old.txt]`), the code should parse both old and new lists.
--- email_list_parser/test.rb
+++ email_list_parser/test.rb
@@ -2,7 +2,7 @@ require "./task.rb"
 require "csv"
 
 describe EmailListParser do
-  let(:parser) { described_class.new("./support/email-list.txt") }
+  let(:parser) { described_class.new("./support/email-list-new.txt") }
 
   before do
     File.delete("results.csv") if File.exist?("results.csv")
@@ -15,11 +15,22 @@ describe EmailListParser do
   end
 
   describe "#process" do
-    it "generate expected results.csv file" do
-      parser.process
-      generated_csv = CSV.read("results.csv")
-      expected_csv = CSV.read("support/results.csv")
-      expect(generated_csv).to eq expected_csv
+    context "new email list" do
+      it "generate expected results.csv file" do
+        parser.process
+        generated_csv = CSV.read("results.csv")
+        expected_csv = CSV.read("support/results-new.csv")
+        expect(generated_csv).to eq expected_csv
+      end
+    end
+
+    context "old email list" do
+      it "generate expected results.csv file" do
+        described_class.new("./support/email-list-old.txt").process
+        generated_csv = CSV.read("results.csv")
+        expected_csv = CSV.read("support/results-old.csv")
+        expect(generated_csv).to eq expected_csv
+      end
     end
   end
 end
